0e455d3c232bb5a8b5060e766bf34ecc2ffd892c
Daniel Cruz <dcruz.js@gmail.com>
2020-08-05 15:33:07 -0600

    Updates

---
+++ precache-manifest.343c0d54ee80f2963f2720ccb9b4108c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c8a087b59dda2160575882462a0bbd8",
+    "url": "/social-media-dashboard/index.html"
+  },
+  {
+    "revision": "1392a14068e85f948fc9",
+    "url": "/social-media-dashboard/static/css/main.cdc227b7.chunk.css"
+  },
+  {
+    "revision": "0040656ae50036dfcab0",
+    "url": "/social-media-dashboard/static/js/2.27091492.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/social-media-dashboard/static/js/2.27091492.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1392a14068e85f948fc9",
+    "url": "/social-media-dashboard/static/js/main.5899c7bb.chunk.js"
+  },
+  {
+    "revision": "3b1ddb8a90a9326f6c18",
+    "url": "/social-media-dashboard/static/js/runtime-main.9d9f00af.js"
+  }
+]);
